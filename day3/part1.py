2e847b5ce0625f3dd9a34cf55168ed7d7dee3397
av-124 <adithya.vignaraja@gmail.com>
2023-12-04 23:03:53 +1100

    add day4 part1 solution and input.txt

--- day3/part1.py
+++ day3/part1.py
@@ -5,5 +5,40 @@
 # Alternative approach: Cycle through each digit in the number and scan 3 x 3 area for symbol
 # The numbers that don't detect a symbol after the scan can be removed from the list
 # Remaining numbers from the list can be summed -> do this for each row
-# Symbols = @, #, $, %, &, *, -, +, =, / (10 in total)
+# Symbols: @, #, $, %, &, *, -, +, =, / (10 in total)
 
+# Matrix coordinates would be as follows:
+# (0, 0), (0, 1), (0, 2) ...
+# (1, 0), (1, 1), (1, 2) ...
+# (2, 0), (2, 1), (2, 2) ...
+# ...
+
+# Ignoring the rows 0 & n and cols 0 & n, the scan area can be expressed as this general solution:
+
+# scan_coords = [-1, 0, 1]
+
+# Use pandas to organise into a matrix, use size function to determine rows and cols in input data
+# for a given coordinate (r, c), scan through using these two nested for loops:
+# for r in range(1, rows-1):
+    # for c in range(1, cols-1):
+
+import re
+import pandas
+
+symbols = {'@', '#', '$', '%', '&', '*', '-', '+', '=', '/'}
+
+
+
+# Specify the path to your text file
+file_path = 'input.txt'
+
+# Open the file in read mode and iterate through each line
+with open(file_path, 'r') as file:
+    
+    for line in file:
+        
+        nums_to_keep = re.findall(r'\d+', line)
+
+        for i in range(0, len(line)):
+            if line[i] in symbols:
+                print(len(line))
---
+++ day4/part1.py
@@ -0,0 +1,38 @@
+# Find number of common elements in each set
+# If there is a match, points per card = 2 ^ (matches - 1)
+# No match = 0 points
+
+def points_per_card(input_data):
+        
+    # Data cleaning
+    numbers = input_data.split(': ')[1]
+    numbers = numbers.replace('  ', ' ')    # replace double spaces with a single space
+    winning_nums = numbers.split(' | ')[0]
+    your_nums = numbers.split(' | ')[1]
+
+    winning_set = set(winning_nums.strip().split(' '))
+    your_set = set(your_nums.strip().split(' '))
+    print(winning_set & your_set)
+
+    no_of_matches = int(len(winning_set & your_set))
+    print(no_of_matches)
+
+    if no_of_matches > 0:
+        points = 2 ** (no_of_matches - 1)
+    else:
+        points = 0
+    return points
+
+# Specify the path to your text file
+file_path = 'input.txt'
+
+# Open the file in read mode and iterate through each line
+with open(file_path, 'r') as file:
+    total = 0
+    i = 0
+
+    for line in file:
+        total += points_per_card(line)
+        print('Cards counted so far = ' + str(i))
+        print('Total sum so far = ' + str(total) + '\n')
+        i += 1
