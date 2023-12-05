982c5ff9ce5225377d5181302be35449f5c7f396
av-124 <adithya.vignaraja@gmail.com>
2023-12-05 18:52:42 +1100

    work on day3 part1 solution

--- day3/part1.py
+++ day3/part1.py
@@ -1,44 +1,123 @@
 # Need to keep track of the location of each symbol and scan the surrounding 3 x 3 area for numbers
-# Need to decide how many rows of the input to scan each time – this will depend on the number of rows in total
-# Might also need to strip out all of the numbers from each line and store in a list
-
-# Alternative approach: Cycle through each digit in the number and scan 3 x 3 area for symbol
-# The numbers that don't detect a symbol after the scan can be removed from the list
+# Might also need to get the numbers from each line and store in a list
+# The numbers that aren't detected by a symbol after the scan can be removed from the list
 # Remaining numbers from the list can be summed -> do this for each row
 # Symbols: @, #, $, %, &, *, -, +, =, / (10 in total)
 
-# Matrix coordinates would be as follows:
-# (0, 0), (0, 1), (0, 2) ...
-# (1, 0), (1, 1), (1, 2) ...
-# (2, 0), (2, 1), (2, 2) ...
+# Based on the input data, assume no symbols will be present on any of the edges / borders
+
+# Matrix coordinates to scan would be as follows:
+# (r-1, c-1), (r-1, c), (r-1, c+1) ...
+# (r, c-1),   (r, c),   (r, c+1)   ...
+# (r+1, c-1), (r+1, c), (r+1, c+1) ...
 # ...
 
-# Ignoring the rows 0 & n and cols 0 & n, the scan area can be expressed as this general solution:
+import re
+import pandas as pd
 
-# scan_coords = [-1, 0, 1]
+symbols = {'@', '#', '$', '%', '&', '*', '-', '+', '=', '/'}
+
+def extract_digits(string, position):
+    # Check if the given position is within the bounds of the string
+    if position < 0 or position >= len(string):
+        return "Position out of bounds"
+
+    # Initialize variables to store the result
+    result = ""
+    
+    # Iterate left from the given position
+    for i in range(position, -1, -1):
+        # If the character is a digit, add it to the result
+        if string[i].isdigit():
+            result = string[i] + result
+        # If the character is a dot, stop iterating
+        elif string[i] == '.':
+            break
+        # If the character is not a digit or dot, stop iterating
+        else:
+            break
+    
+    # Iterate right from the given position
+    for i in range(position + 1, len(string)):
+        # If the character is a digit, add it to the result
+        if string[i].isdigit():
+            result += string[i]
+        # If the character is a dot, stop iterating
+        elif string[i] == '.':
+            break
+        # If the character is not a digit or dot, stop iterating
+        else:
+            break
+    return result
+
+def scan_surrounding_positions(r, c):
+    
+    for i in range(r - 1, r + 2):
+        for j in range(c - 1, c + 2):
+            # Need to locate the digits and return something useful – might need to include another argument in the function
+
+            # Skip the current position (r, c)
+            if i == r and j == c:
+                continue
+            
+            # Process or scan the position (i, j)
+            print(f"Scanning position: ({i}, {j})")
 
 # Use pandas to organise into a matrix, use size function to determine rows and cols in input data
 # for a given coordinate (r, c), scan through using these two nested for loops:
 # for r in range(1, rows-1):
     # for c in range(1, cols-1):
+        # if matrix[r][c] in symbols:
+            # Scan 3 x 3 area to see if there are any numbers (MAKE SURE TO AVOID DOUBLE COUNTING)
 
-import re
-import pandas
+def make_df(input_data_file_path):
 
-symbols = {'@', '#', '$', '%', '&', '*', '-', '+', '=', '/'}
+    # Read the content of the file
+    with open(input_data_file_path, 'r') as file:
+        input_data = file.read()
+        lines = input_data.split('\n')
+
+        # Create a list of lists representing the matrix
+        matrix = [list(line) for line in lines]
+
+        # Create a DataFrame from the matrix
+        df = pd.DataFrame(matrix)
+        return df
 
 
+data = make_df('input.txt')
+rows, cols = data.shape
+print(data)
 
-# Specify the path to your text file
-file_path = 'input.txt'
 
-# Open the file in read mode and iterate through each line
-with open(file_path, 'r') as file:
-    
-    for line in file:
+for r in range(1, rows-1):
+    for c in range(1, cols-1):
+        print(data.iloc[r, c])
         
-        nums_to_keep = re.findall(r'\d+', line)
+        if data.iloc[r, c] in symbols:
+            # Scan 3 x 3 area
+            for i in range(r - 1, r + 2):
+                for j in range(c - 1, c + 2):
+                # Need to locate the digits and return something useful – might need to include another argument in the function
 
-        for i in range(0, len(line)):
-            if line[i] in symbols:
-                print(len(line))
+                    # Skip the current position (r, c)
+                    if i == r and j == c:
+                        continue
+                    
+                    # Process or scan the position (i, j)
+                    print(f"Scanning position: ({i}, {j})")
+
+
+# # Specify the path to your text file
+# file_path = 'input.txt'
+
+# # Open the file in read mode and iterate through each line
+# with open(file_path, 'r') as file:
+    
+#     for line in file:
+        
+#         nums_to_keep = re.findall(r'\d+', line)
+#         # print(nums_to_keep)
+
+#         # for i in range(0, len(line)):
+#         #     if line[i] in symbols:
